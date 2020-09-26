99be217b955e3b4d593e8acfdd4bf59536890d71
Kriskriskris <kristin.eliassen@gmail.com>
2020-09-26 20:40:13 +0200

    Removed country from location title

--- src/City.js
+++ src/City.js
@@ -4,9 +4,7 @@ import "./City.css";
 export default function City(props) {
   return (
     <div className="City">
-      <h1>
-        {props.cityVal} <span> {props.countryVal}</span>
-      </h1>
+      <h1>{props.cityVal}</h1>
       <h3>Broken clouds</h3>
       <p>Last updated on Tuesday at 19:50</p>
     </div>
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./SearchEngine.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default function SearchEngine() {
+  let [city, setCity] = useState(null);
+
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q={${city}&appid=84fd1cfe085aae87f6eca82b4b8c991a&units=metric`;
+    axios.get(url).then(updateWeather);
+  }
+
+  function updateWeather(response) {
+    let temperature = response.data.main.temp;
+    let description = response.data.weather[0].description;
+    let humidity = response.data.main.humidity;
+    let wind = Math.round(response.data.wind.speed);
+  }
+
   return (
     <div className="SearchEngine">
       <form>
         <input
-          type="text"
+          type="search"
           id="searchField"
           className="search-input"
           placeholder="Enter a city"
           autoComplete="off"
           autoFocus
+          onSubmit={handleSubmit}
         />
-        <button type="submit" className="formBtn search-button " id="searchBtn">
+        <button
+          type="submit"
+          className="formBtn search-button "
+          id="searchBtn"
+          onChange={updateCity}
+        >
           <FontAwesomeIcon icon={faSearch} />
         </button>
         <button value="Local" className="formBtn" id="currentLocationBtn">
