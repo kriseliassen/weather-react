120b4a6d0fe7292ae99fe91f8e469499aec9df98
Kriskriskris <kristin.eliassen@gmail.com>
2020-09-26 20:22:47 +0200

    Added LinkedIn-link to Credit-section

--- src/Credit.js
+++ src/Credit.js
@@ -4,10 +4,13 @@ import "./Credit.css";
 export default function Credit() {
   return (
     <div className="Credit">
-      <a href="https://github.com/kriseliassen/Weather-app">
+      <a href="https://github.com/kriseliassen/weather-react">
         Open source code{" "}
       </a>
-      by Kristin Eliassen
+      by{" "}
+      <a href="https://www.linkedin.com/in/kristineliassen/">
+        Kristin Eliassen
+      </a>
     </div>
   );
 }
