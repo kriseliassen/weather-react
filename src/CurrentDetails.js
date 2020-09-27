b229c044d26555669e90f008e1548e44c16843fc
Kriskriskris <kristin.eliassen@gmail.com>
2020-09-27 15:38:59 +0200

    Added celsius/fahrenheit conversion, removed max/min temp

--- src/CurrentDetails.js
+++ src/CurrentDetails.js
@@ -5,11 +5,15 @@ export default function CurrentDetails(props) {
   return (
     <div className="CurrentDetails">
       <ul>
-        <li>Humidity: {props.humidityVal}%</li>
-        <li>Wind: {props.windVal}m/s</li>
         <li>
-          Max/min: <br></br>
-          {props.maxTempVal}°C | {props.minTempVal}°C
+          <strong>Humidity: </strong>
+          <br />
+          {props.humidityVal}%
+        </li>
+        <li>
+          <strong>Wind: </strong>
+          <br />
+          {props.windVal} m/s
         </li>
       </ul>
     </div>
--- src/Temperature.js
+++ src/Temperature.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Temperature.css";
 
 export default function Temperature(props) {
-  return (
-    <div className="Temperature">
-      <strong>{props.temperatureVal}</strong>
-      <div className="units">
-        <button className="unitBtn active">°C</button>
-        <button className="unitBtn">°F</button>
+  const [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    fahrenheit = (props.temperatureVal * 9) / 5 + 32;
+    return Math.round(fahrenheit);
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="Temperature">
+        <strong>{props.temperatureVal}</strong>
+        <div className="units">
+          <span className="unitBtn active">°C</span>
+          <a href="/" className="unitBtn inactive" onClick={showFahrenheit}>
+            °F
+          </a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="Temperature">
+        <strong>{fahrenheit()}</strong>
+        <div className="units">
+          <a href="/" className="unitBtn inactive" onClick={showCelsius}>
+            °C
+          </a>
+          <span className="unitBtn active">°F</span>
+        </div>
+      </div>
+    );
+  }
 }
